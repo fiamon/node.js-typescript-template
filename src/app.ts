c8e0916a4f90ff5c48ed21f3af14003e0df1ebba
fiamon <juliogustavofi@gmail.com>
2024-01-24 18:13:20 -0300

    refactor: routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import { routes } from './routes';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+
+app.use(routes);
 
 export default app;
---
+++ src/controllers/user.ts
@@ -0,0 +1,5 @@
+export default class UserController {
+    getAll(): string {
+        return 'hello';
+    }
+}
